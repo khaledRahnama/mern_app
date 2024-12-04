9a1ef8910758fd73142d2e937bea410d088fffe0
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-12-04 22:52:22 +0100

    add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,34 @@ function App() {
 
 
   return (
+    <header className="flex justify-between ">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 rotate-180 w-8 h-8  ">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
+      </svg>
+      <span className="font-normal text-xl ml-1 mt-1">airbnb</span>
 
-    <div className='bg-red-500'>test</div>
+      <div className="flex gap-2 border border-gray-300 rounded-full py-1 px-2 shadow-md shadow-gray-300 items-center ">
+        <div className="border-r border-gray-300 pr-2  sm:text-sm  lg:text-xl text-clip overflow-hidden">Anywhere</div>
+        <div className="border-r border-gray-300 pr-2 teext-eclipsis sm:text-sm  lg:text-xl"> Any time</div>
+        <div className="sm:text-sm  lg:text-xl overflow: hidden; text-overflow: ellipsis;
+                 white-space: nowrap;">Add guests</div>
+
+        <button className=" bg-special-red text-white rounded-full p-1 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+
+        </button>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+        <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+      </svg>
+
+    </header>
 
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: [ "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'special-red':'#F5385D',
+
+      }
+    },
   },
   plugins: [],
 }
